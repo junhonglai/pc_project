dd4336bd43b62b3e5b081ff221d6f18f7d04a28b
咸鱼翻身还是咸鱼 <8502966+laijunhong@user.noreply.gitee.com>
2021-06-21 16:27:13 +0800

    路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,42 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/Home'
+import Search from '../views/Search'
+import Login from '../views/Login'
+import Register from '../views/Register'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
     name: 'Home',
+    path: '/',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    name: 'Search',
+    path: '/search',
+    component: Search
+  },
+  {
+    name: 'Login',
+    path: '/login',
+    component: Login
+  },
+  {
+    name: 'Register',
+    path: '/register',
+    component: Register
+  },
+  
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
