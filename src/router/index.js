8178617624dd837f40eb185365601b0f5bece4ee
咸鱼翻身还是咸鱼 <8502966+laijunhong@user.noreply.gitee.com>
2021-07-02 17:41:59 +0800

    完成路由懒加载和路由守卫

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,43 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home";
-import Search from "../views/Search";
-import Login from "../views/Login";
-import Register from "../views/Register";
-import Detail from "../components/Detail";
-import AddCartSuccess from "../views/AddCartSuccess";
-import ShopCart from '../views/ShopCart'
-import Trade from '../views/Trade'
-import Pay from '../views/Pay'
-import PaySuccess from '../views/PaySuccess'
-import Center from '../views/Center'
+// import Home from "../views/Home";
+// import Search from "../views/Search";
+// import Login from "../views/Login";
+// import Register from "../views/Register";
+// import Detail from "../components/Detail";
+// import AddCartSuccess from "../views/AddCartSuccess";
+// import ShopCart from '../views/ShopCart'
+// import Trade from '../views/Trade'
+// import Pay from '../views/Pay'
+// import PaySuccess from '../views/PaySuccess'
+// import Center from '../views/Center'
+
+const Home = () => import(/* webpackChunkName: 'Home'*/ "../views/Home");
+
+const Search = () => import(/* webpackChunkName: 'Search'*/ "../views/Search");
+
+const Login = () => import(/* webpackChunkName: 'Login'*/ "../views/Login");
+
+const Register = () =>
+  import(/* webpackChunkName: 'Register'*/ "../views/Register");
+
+const Detail = () =>
+  import(/* webpackChunkName: 'Detail'*/ "../components/Detail");
+
+const AddCartSuccess = () =>
+  import(/* webpackChunkName: 'AddCartSuccess'*/ "../views/AddCartSuccess");
+
+const ShopCart = () =>
+  import(/* webpackChunkName: 'ShopCart'*/ "../views/ShopCart");
+
+const Trade = () => import(/* webpackChunkName: 'Trade'*/ "../views/Trade");
+
+const Pay = () => import(/* webpackChunkName: 'Pay'*/ "../views/Pay");
+
+const PaySuccess = () =>
+  import(/* webpackChunkName: 'PaySuccess'*/ "../views/PaySuccess");
+
+const Center = () => import(/* webpackChunkName: 'Center'*/ "../views/Center");
 
 Vue.use(VueRouter);
 
@@ -60,16 +87,43 @@ const routes = [
     name: "Trade",
     path: "/trade",
     component: Trade,
+    beforeEnter(to, from, next) {
+      // 判断是否是从ShopCart进入的Trade
+      if (from.name === "ShopCart") {
+        next();
+        return;
+      }
+      // 不是的话返回来的页面
+      next(from);
+    },
   },
   {
     name: "Pay",
     path: "/pay",
     component: Pay,
+    beforeEnter(to, from, next) {
+      // 判断是否是从Trade进入的Pay
+      if (from.name === "Trade") {
+        next();
+        return;
+      }
+      // 不是的话返回来的页面
+      next(from);
+    },
   },
   {
     name: "PaySuccess",
     path: "/paysuccess",
     component: PaySuccess,
+    beforeEnter(to, from, next) {
+      // 判断是否是从Pay进入的PaySuccess
+      if (from.name === "Pay") {
+        next();
+        return;
+      }
+      // 不是的话返回来的页面
+      next(from);
+    },
   },
   {
     name: "Center",
